46f290c9e1935ae968853f9868fd30f6db63aa7d
Yumadilov Timur <timurice@mail.ru>
2018-08-15 19:35:32 +0500

    Добавил виджеты

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Раскрывающаяся навигация
 function hamburgerMenu() {
 
   const hambMenuOpenBtn = document.querySelector(".header__hambmenu-btn");
@@ -33,3 +34,131 @@ function hamburgerMenu() {
 }
 hamburgerMenu();
 
+// Вертикальный аккордеон в секции "Команда"
+function accordeonTeam() {
+
+  const memberBtn = document.querySelectorAll(".team__name");
+  const teamList = document.querySelector(".team__list");
+
+  teamList.addEventListener("click", function (event) {
+    let target = event.target;
+    if (target.classList.contains("team__name")) {
+
+      for (let i = 0; i < memberBtn.length; i++) {
+        if (memberBtn[i] != target) {
+          memberBtn[i].classList.remove("team__name--active");
+        }
+      }
+
+      if (target.classList.contains("team__name--active")) {
+        target.classList.remove("team__name--active");
+      } else {
+        target.classList.add("team__name--active");
+      }
+
+    }
+    return;
+  });
+
+}
+accordeonTeam();
+
+// Горизонтальный аккордеон в секции "Меню"
+function accordeonMenu() {
+
+  const menuBtn = document.querySelectorAll(".menu__cover");
+  const menuList = document.querySelector(".menu__list");
+  const menuSpan = document.querySelectorAll(".menu__type-title");
+
+  menuList.addEventListener("click", function (event) {
+    var target = event.target;
+    if (target.classList.contains("menu__cover")) {
+
+      for (let i = 0; i < menuBtn.length; i++) {
+        if (menuBtn[i] != target) {
+          menuBtn[i].classList.remove("menu__cover--active");
+        }
+      }
+
+      if (target.classList.contains("menu__cover--active")) {
+        target.classList.remove("menu__cover--active");
+      } else {
+        target.classList.add("menu__cover--active");
+      }
+
+    }
+
+    if (target.classList.contains("menu__type-title")) {
+
+      for (let i = 0; i < menuBtn.length; i++) {
+        if (menuBtn[i] != target.parentElement) {
+          menuBtn[i].classList.remove("menu__cover--active");
+        }
+      }
+
+      if (target.parentElement.classList.contains("menu__cover--active")) {
+        target.parentElement.classList.remove("menu__cover--active");
+      } else {
+        target.parentElement.classList.add("menu__cover--active");
+      }
+      
+    }
+    return;
+  });
+
+}
+accordeonMenu();
+
+// Popup в секции "Отзывы"
+const reviewBtn = document.querySelectorAll(".buy-button--more");
+
+for (let i = 0; i < reviewBtn.length; i++) {
+reviewBtn[i].addEventListener("click", function(event) {
+  var reviewTextContent = reviewBtn[i].previousElementSibling.textContent;
+  var reviewTitle = reviewBtn[i].parentElement.children[0].textContent;
+  document.body.appendChild(openPopup(reviewTextContent, reviewTitle));
+  document.body.style.overflow = "hidden";
+  var reviewPopupOpacity = document.querySelector(".review-popup");
+  if (reviewPopupOpacity) {
+    reviewPopupOpacity.style.opacity = getComputedStyle(reviewPopupOpacity).opacity + 1;
+  }
+});
+}
+
+function openPopup(content, title) {
+  const reviewPopup = document.createElement("div");
+  reviewPopup.classList.add("review-popup");
+
+  const reviewContainer = document.createElement("div");
+  reviewContainer.classList.add("review-popup__container");
+
+  const reviewName = document.createElement("h3");
+  reviewName.classList.add("review-popup__name");
+  reviewName.textContent = title;
+
+  const reviewContent = document.createElement("p");
+  reviewContent.classList.add("review-popup__text");
+  reviewContent.textContent = content;
+
+  const closePopup = document.createElement("button");
+  closePopup.classList.add("review-popup__close-btn");
+
+  closePopup.addEventListener("click", function() {
+    var reviewPopupOpacity = document.querySelector(".review-popup");
+    function removePopup() {
+      document.body.removeChild(reviewPopup);
+    }
+    if (reviewPopupOpacity) {
+      reviewPopupOpacity.style.opacity = getComputedStyle(reviewPopupOpacity).opacity - 1;
+    }
+    setTimeout(removePopup, 600);
+    document.body.style.overflow = "visible";
+  });
+
+  reviewPopup.appendChild(reviewContainer);
+  reviewContainer.appendChild(reviewName);
+  reviewContainer.appendChild(reviewContent);
+  reviewContainer.appendChild(closePopup);
+
+  return reviewPopup;
+}
